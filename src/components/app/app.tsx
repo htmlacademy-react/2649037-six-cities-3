99de2e64f847834c346942a097021c8742e778ac
IEROGLIF <ieroglif.kerch@gmail.com>
2026-08-24 19:54:26 +0300

    - переименовал файлы под общий шаблон - добавил роутинг для всех страниц и приватный для любимых - добавил файл с констатнами адресов и авторизации - добавил страницу 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,57 @@
-import MainPage from '../../pages/main-page/MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import MainPage from '../../pages/main-page/main-page';
+
+import PrivateRoute from '../../components/private-router/private-router';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
 type AppProps = {
   offersCount: number;
 };
+
 function App({ offersCount }: AppProps): JSX.Element {
-  return <MainPage offersCount={offersCount} />;
+  const authorizationStatus = AuthorizationStatus.NoAuth; // пока всегда не авторизован
+
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage offersCount={offersCount} />}
+        />
+
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={authorizationStatus}>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
